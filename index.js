7d5b57241a27a8ae1063f2a1379a2ddf5ae54066
Stephen Whitmore <sww@google.com>
2014-11-07 18:12:16 -0800

    Uses modules for tempfiles and mkfifo.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var http = require('http');
 var youtubedl = require('youtube-dl');
 var mplayer = require('node-mplayer');
 var fs = require('fs');
+var mkfifo = require('mkfifo').mkfifoSync;
+var tmp = require('tmp');
 
 var router = routes();
 router.addRoute("/play/:url", playUrl);
 router.addRoute("/*", fourOhFour);
 
 http.createServer(function(req, res) {
-  console.dir(req.url);
-
   var route = router.match(req.url);
   if (route) {
     route.fn.apply(null, [req, res, route.params, route.splats]);
@@ -20,11 +20,61 @@ http.createServer(function(req, res) {
 
 function playUrl(req, res, params, splats) {
   var video = youtubedl(params.url, ['--max-quality=18'], {});
-  video.pipe(fs.createWriteStream('/tmp/video'));
-  video.on('end', function() {
-    var player = new mplayer('/tmp/video');
+
+  tmp.tmpName(function(err, fifoName) {
+    if (err) {
+      throw err;
+      res.writeHead(500, "tmpName messed up");
+      res.write(err);
+      res.end();
+    }
+
+    try {
+      mkfifo(fifoName, 0755);
+    } catch (e) {
+      res.writeHead(500, "mkfifo messed up");
+      res.write(e);
+      res.end();
+    }
+    console.error("Created fifo: " + fifoName);
+    var fifo = fs.createWriteStream(fifoName);
+
+    console.error("Starting to download " + params.url);
+
+    video.pipe(fifo);
+
+    var player = new mplayer(fifoName);
     player.play();
-    res.end();
+
+    fifo.on('error', function(err) {
+      if (err.code === 'EPIPE') {
+        // perfectly normal; the player was likely killed
+        res.end();
+      } else {
+        console.error("fifo error");
+        console.error(err);
+        res.writeHead(500, 'fifo error');
+        res.write(err);
+        res.end();
+      }
+    });
+    video.on('error', function(err) {
+      console.error("video error");
+      console.error(err);
+      res.writeHead(500, 'video error');
+      res.write(err);
+      res.end();
+    });
+    player.on('error', function(err) {
+      console.error("player error");
+      console.error(err);
+      res.writeHead(500, 'player error');
+      res.write(err);
+      res.end();
+    });
+    player.on('end', function() {
+      res.end();
+    });
   });
 }
 
